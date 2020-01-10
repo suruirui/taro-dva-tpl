a98dc548600cd0c198b83ed596aaf717dae80f8f
suruirui <guimei201314@gmail.com>
2020-01-10 19:02:31 +0800

    api change

--- src/utils/api.js
+++ src/utils/api.js
@@ -12,16 +12,11 @@ const api = {
     refreshCode: AUTH_URL + "/refresh_code" //获取防刷验证码
   },
   //个人中心
-  mine: {
-    info: "/api/usercenter/userinfo/simple", //获取当前登录用户基本信息
-    senceInfo: "/api/usercenter/userinfo/detail", //获取指定用户指定业务下的详细信息
-    idInfo: "/api/usercenter/userinfo/getbyid", //根据用户信息的id获取用户信息
-    updateInfo: "/api/usercenter/userinfo/save" //新增或修改指定业务下的用户信息 返回用户信息id
-  },
+  me: {},
   // 公共服务
   common: {
-    region: "/master-data/api/region/list", //区域列表 省市区
-    upload: "/api/prescription-project/v1/storage/create" // 文件上传
+    region: "", //区域列表 省市区
+    upload: "" // 文件上传
   }
 };
 
